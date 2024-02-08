91a8b04f715387d19a851aa573c2544f242a4f83
LucasT <tressoldi.lucas@gmail.com>
2024-02-08 14:45:52 -0300

    create flavor exceptions

--- src/Flavor.java
+++ src/Flavor.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class Flavor {
     public String chooseFlavor(String drinkType) {
         Scanner scanner = new Scanner(System.in);
-        if (drinkType.equals("suco")) {
+        if (drinkType.equals("Suco")) {
             System.out.print("Uva ou Laranja? ");
+            String response = scanner.nextLine();
+            if (response.equals("Uva") || response.equals("Laranja")) {
+                return response;
+            } else {
+                throw new RuntimeException("Entrada inválida.");
+            }
         } else {
             System.out.print("Coca ou Guaraná? ");
+            String response = scanner.nextLine();
+            if (response.equals("Coca") || response.equals("Guaraná")) {
+                return response;
+            } else {
+                throw new RuntimeException("Entrada inválida.");
+            }
         }
-        return scanner.nextLine();
     }
 }
