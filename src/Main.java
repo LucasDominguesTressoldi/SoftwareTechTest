8d97b5510431d21646b1c95d01037e27a40ae077
LucasT <tressoldi.lucas@gmail.com>
2024-02-08 11:53:23 -0300

    create menu in Main class to choose between juice and soda

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Order juiceOrder = new Order("suco");
-        juiceOrder.prepareOrder();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("[1] Suco | [2] Refrigerante\nQual bebida você deseja? ");
+        if (scanner.nextLine().equals("1")) {
+            Order juiceOrder = new Order("Suco");
+            juiceOrder.prepareOrder();
+        } else {
+            Order sodaOrder = new Order("Refrigerante");
+            sodaOrder.prepareOrder();
+        }
+        System.out.println("-".repeat(26));
+        System.out.println("Obrigado!\nVolte Sempre.");
     }
 }
